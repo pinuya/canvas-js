1075c800323e997e5676df77e19d651374338f4f
Tifany <tifsnunes@gmail.com>
2024-08-06 19:05:33 -0300

    drawing function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //initial data
 let currentColor = "black";
 let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector("#tela");
 let ctx = screen.getContext("2d");
@@ -23,12 +25,35 @@ function colorClickEvent(e) {
   e.target.classList.add("active");
 }
 
-function mouseDownEvent() {
+function mouseDownEvent(e) {
   canDraw = true;
+  mouseX = e.pageX - screen.offsetLeft;
+  mouseY = e.pageY = screen.offsetTopTop;
 }
 
-function mouseMoveEvent() {}
+function mouseMoveEvent() {
+  if (canDraw) {
+    draw(e.pageX, e.pageY);
+  }
+}
 
 function mouseUpEvent() {
   canDraw = false;
+}
+
+function draw(x, y) {
+  let pointX = x - screen.offsetLeft;
+  let pointY = y - screen.offsetTop;
+
+  ctx.beginPath();
+  ctx.lineWidth = 5;
+  ctx.lineJoin = "round";
+  ctx.moveTo(mouseX, mouseY);
+  ctx.lineTo(pointX, pointY);
+  ctx.closePath();
+  ctx.strokeStyle = currentColor;
+  ctx.stroke();
+
+  mouseX = pointX;
+  mouseY = pointY;
 }
